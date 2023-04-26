467bb5e4f8e88559b4bea038759ba3f3466dba8e
MOKA2004 <120271041+MOKA2004@users.noreply.github.com>
2023-04-27 00:02:34 +0530

    Add files via upload

---
+++ whether_it_is_odd_or_even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+   int n;
+   printf("Enter an integer number:\n");
+   scanf("%d",&n);
+   if(n%2==0){
+    printf("%d is an EVEN number.",n);
+   }else{
+   printf("%d is an ODD number.");
+   }
+     
+
+}
